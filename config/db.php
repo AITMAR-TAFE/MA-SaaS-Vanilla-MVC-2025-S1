176e916e8e461cb41baaad0a371fbaba434b7acf
Martina Ait <20114816@tafe.wa.edu.au>
2025-03-12 12:25:18 +0800

    12 March in-class demo

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Database Configuration File
+ *
+ * Filename:        db.php
+ * Location:        config/
+ * Project:         SaaS-Vanilla-MVC
+ * Date Created:    12/03/2025
+ *
+ * Author:          Martina Ait
+ *
+ */
+return [
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'dbname' => 'ma_saas_vanilla_mvc',
+    'username' => 'ma_saas_vanilla_mvc',
+    'password' => 'Password1234'
+];
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'aitmar-tafe/ma_saas_vanilla_mvc_2025_s1',
+        'pretty_version' => '0.1',
+        'version' => '0.1.0.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'aitmar-tafe/ma_saas_vanilla_mvc_2025_s1' => array(
+            'pretty_version' => '0.1',
+            'version' => '0.1.0.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
